22f4987f43070a6492f5e140350b8b3f40ea531e
pro-stoi <123834383+pro-stoi@users.noreply.github.com>
2026-07-02 23:29:01 +0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -234,7 +234,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const rect = document.getElementById('playBtn').getBoundingClientRect();
         spawnParticles(rect.left + rect.width/2, rect.top + rect.height/2, 200);
         setTimeout(() => {
-            goToWithAd('game.html');  // ← РЕКЛАМА ТОЛЬКО ЗДЕСЬ
+            goTo('game.html');  // ← РЕКЛАМА ТОЛЬКО ЗДЕСЬ
         }, 300);
     });
 
